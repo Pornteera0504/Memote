628b81d1bd28b23faf123fbe1ca6198e61c69811
Pornteera Pakdee <56480313+Pornteera0504@users.noreply.github.com>
2022-04-23 16:03:03 +0700

    Add logout api and refactor code

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, logoutUser } = require('../controllers/userController')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+router.post('/logout', logoutUser)
 
 module.exports = router
