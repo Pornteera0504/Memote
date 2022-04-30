c060fcd215b9d2134fde6434a06f25105abc978f
Pornteera Pakdee <56480313+Pornteera0504@users.noreply.github.com>
2022-04-30 17:09:14 +0700

    add api for add/Edit task data detail. and update mock task data

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -29,4 +29,66 @@ const taskDetail = async (req, res) => {
     }
 }
 
-module.exports = { taskDetail }
+const validateCategory = async (categoryName,conn) => {
+    querySql = "SELECT categoryID FROM categories where name = ?"
+    const [row, _] = await conn.query(querySql, [categoryName])
+
+    if(row.length === 0) return res.status(400).send("This category is not already.")
+    const categoryID =  row[0].categoryID
+    return categoryID
+}
+
+const modifyTask = async (req, res) => {
+    try {
+        const conn = await pool.getConnection()
+        await conn.beginTransaction()
+
+        var today = new Date();
+        var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear()
+
+        var statusModify
+        const taskID = req.body.taskID
+        const userID = req.body.userID
+        const name = req.body.name
+        const description = req.body.description
+        const activityDate = req.body.activityDate
+        const lastEditDate = date
+        const categoryName = req.body.categoryName
+        
+        try {
+            querySql = "SELECT * FROM users where userID = ?"
+            const [row, _] = await conn.query(querySql, [userID])
+            if(row.length === 0) return res.status(400).send("This user not have permission.")
+
+            if(taskID === 0 ){
+                const categoryID = await validateCategory(categoryName, conn)
+                const insertSql = "INSERT INTO tasks (name, description, activityDate, lastEditDate, userID, categoryID) VALUES (?, ?, ?, ?, ?, ?)"
+                await conn.query(insertSql, [name, description, activityDate, lastEditDate, userID, categoryID])
+                statusModify = "Add task data success."
+                res.status(200).send(statusModify)
+            }
+            else{
+                const categoryID = await validateCategory(categoryName, conn)
+                console.log(categoryID)
+                const updateSql = "UPDATE tasks SET name = ?, description = ?, activityDate = ?, lastEditDate = ?, categoryID = ? WHERE taskID = ?"
+                await conn.query(updateSql, [name, description, activityDate, lastEditDate, categoryID, taskID])
+                statusModify = "Update task data success."
+                res.status(200).send(statusModify)
+            }
+
+            await conn.commit()
+            return statusModify
+            
+        } catch (error) {
+            await conn.rollback()
+            return res.status(500).send("[Modify task module] Can not modify task data")
+        } finally {
+            conn.release()
+        }
+          
+    } catch (error) {
+        return res.status(500).send("[Modify task module] Can not get database connection")
+    }
+}
+
+module.exports = { taskDetail, modifyTask }
--- backend/routers/taskRouter.js
+++ backend/routers/taskRouter.js
@@ -2,8 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
-const { taskDetail } = require('../controllers/taskController')
+const { taskDetail, modifyTask } = require('../controllers/taskController')
 
 router.get('/task/detail', taskDetail)
+router.post('/task/modify', modifyTask)
 
 module.exports = router
