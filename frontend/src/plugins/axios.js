aba045bced626ead312ce11a565347ef5123772f
Bordee Jakteerangkul <51290396+Bordee2000@users.noreply.github.com>
2022-05-04 20:13:58 +0700

    fix ip

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://54.221.180.235:3000",
+  baseURL: "http://52.202.97.31:3000",
 });
 
 instance.interceptors.request.use(
