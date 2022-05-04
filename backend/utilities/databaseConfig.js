a690f5b5ea1b66e9c0bf58eece6fd983c955ee6f
Bordee Jakteerangkul <51290396+Bordee2000@users.noreply.github.com>
2022-05-04 15:39:12 +0700

    add dbconfig

--- backend/utilities/databaseConfig.js
+++ backend/utilities/databaseConfig.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise')
 require('dotenv').config({ path: '../.env'})
 
 const pool = mysql.createPool({
-    host: process.env.DBhost || "localhost",
-    user: process.env.DBuser || "root",
-    password: process.env.DBpassword || "101043",
+    host: process.env.DBhost || "memote.czmyyf1cpqym.us-east-1.rds.amazonaws.com",
+    user: process.env.DBuser || "admin",
+    password: process.env.DBpassword || "memote_password",
     database: process.env.DBname || "memote",
     waitForConnections: true,
     connectionLimit: 10,
